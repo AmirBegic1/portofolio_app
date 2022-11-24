b7678b902e11cef56ceedab1b4586ce13923aa67
Amir Begić <amir.begic2019@size.ba>
2022-11-24 02:05:12 +0100

    adding features on mobile

--- lib/mobile/landing_page_mobile.dart
+++ lib/mobile/landing_page_mobile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:portofolio/components.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LandingPageMobile extends StatefulWidget {
   LandingPageMobile({Key? key}) : super(key: key);
@@ -10,6 +13,145 @@ class LandingPageMobile extends StatefulWidget {
 class _LandingPageMobileState extends State<LandingPageMobile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var widhtDevice = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        iconTheme: IconThemeData(
+          size: 35.0,
+          color: Colors.black,
+        ),
+      ),
+      endDrawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 2.0, color: Colors.black),
+                ),
+                child: Image.asset("assets/amir.jpeg"),
+              ),
+            ),
+            TabsMobile(text: "Home", route: '/'),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Works", route: '/works'),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Blog", route: '/blog'),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "About", route: '/'),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Contact", route: '/'),
+            SizedBox(height: 40.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () async =>
+                      await launch("https://www.instagram.com/amir.begic_/"),
+                  icon: SvgPicture.asset(
+                    "assets/instagram.svg",
+                    color: Colors.black,
+                    width: 35.0,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () async =>
+                      await launch("https://github.com/AmirBegic1"),
+                  icon: SvgPicture.asset(
+                    "assets/github.svg",
+                    color: Colors.black,
+                    width: 35.0,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () async => await launch(
+                      "https://www.linkedin.com/in/amir-begi%C4%871/"),
+                  icon: SvgPicture.asset(
+                    "assets/twitter.svg",
+                    color: Colors.black,
+                    width: 35.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: [
+          //intro First section
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 117.0,
+                  backgroundColor: Colors.red,
+                  child: CircleAvatar(
+                    radius: 110.0,
+                    backgroundColor: Colors.black,
+                    backgroundImage: AssetImage("assets/amir.jpeg"),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: SansBold("Hello I'm", 15.0),
+                    ),
+                    SansBold("Amir Begić", 40.0),
+                    Sans("Software engineer", 30.0),
+                    Sans("- Flutter developer - ", 23.0),
+                  ],
+                ),
+                SizedBox(height: 15.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Wrap(
+                      direction: Axis.vertical,
+                      spacing: 3.0,
+                      children: [
+                        Icon(Icons.email),
+                        Icon(Icons.call),
+                        Icon(Icons.location_pin),
+                      ],
+                    ),
+                    SizedBox(width: 40.0),
+                    Wrap(
+                      direction: Axis.vertical,
+                      spacing: 9.0,
+                      children: [
+                        Sans("amir.begic12@gmail.com", 15.0),
+                        Sans("+387 62 653 693", 15.0),
+                        Sans("Zenica, BiH", 15.0),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
